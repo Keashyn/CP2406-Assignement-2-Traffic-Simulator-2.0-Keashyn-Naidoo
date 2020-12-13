562da5a1dda2ccd23f5429f53e6fd6728ca46703
keashynnaidoo <keashyn.naidoo@my.jcu.edu.au>
2020-12-13 13:24:38 +0800

    intial commit

--- src/Test/RoadTest.java
+++ src/Test/RoadTest.java
@@ -1,10 +1,11 @@
+package Test;
+
 import Model.Road;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
 import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
 
 class RoadTest {
     Model.Road road = new Model.Road("0", 1, 5, new int[]{0, 0}, Road.Orientation.HORIZONTAL);
--- src/Test/TrafficLightTest.java
+++ src/Test/TrafficLightTest.java
@@ -1,10 +1,8 @@
-package test;
+package Test;
 
 import Model.Road;
 import Model.TrafficLight;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TrafficLightTest {
     Road road = new Road("0", 1, 5, new int[]{0, 0}, Road.Orientation.VERTICAL);
