dff0748be9148f7a14efcae7e234742013990211
keashynnaidoo <keashyn.naidoo@my.jcu.edu.au>
2020-12-15 09:42:40 +0800

    intial commit

--- src/Bus.java
+++ src/Bus.java
@@ -1,2 +1,13 @@
-public class Bus {
+import java.awt.*;
+
+public class Bus extends Vehicle{
+    public Bus (int x, int y){
+        super(x,y);
+        width=45;
+        height=10;
+        speed=7;
+    }
+    public void paintme(Graphics g){
+
+    }
 }
